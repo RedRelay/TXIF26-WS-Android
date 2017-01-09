0694a8b39eb01a1e3f8878f1f7931beebd6b1d68
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2017-01-09 20:28:56 +0100

    Initial

---
+++ README.txt
@@ -0,0 +1,8 @@
+Il faut node.js installé.
+
+Pour installer les librairies externe : ouvrir node command line, se mettre dans le repertoire webservice-test avec 'cd' puis lancer :
+npm install restify
+npm install restify-errors
+
+Pour lancer le webservice, ouvrir node command line, se mettre dans le repertoire webservice-test avec 'cd' puis lancer :
+node webservice-test
---
+++ webservice-test.js
@@ -0,0 +1,77 @@
+var restify = require('restify');
+var errs = require('restify-errors');
+
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+var LOGIN = "Marcel";
+var PASS = "UTT2030";
+var TOKEN = "523DA4B598A24F6DC4F68B8B237FA";
+var ID_CHAUFFEUR = '4';
+
+
+server.post('/mes_livraisons', function(req, res, next) {
+
+  console.log("received : /mes_livraison : "+JSON.stringify(req.params));
+
+  if(!req.params.TOKEN || req.params.TOKEN !== TOKEN) {
+    res.send(new errs.UnauthorizedError("Bad token"));
+  }else if(!req.params.FROM_ID || req.params.FROM_ID == 0){
+      res.send({ livraisons : [
+          {
+            idLivraison: '1',
+            libelleLivraison: "Supermarché",
+            lat: '48.299953',
+			lng: '4.096061',
+            dateDeFin: '06/12/2017',
+            statusActuel: '1'
+          },
+          {
+            idLivraison: '2',
+            libelleLivraison: "marché",
+            lat: '48.297053',
+			lng: '4.076061',
+            dateDeFin: '08/12/2017',
+            statusActuel: '2'
+          }
+        ]});
+  }else {
+	res.send({ livraisons : []});
+  }
+  return next();
+});
+
+server.post('/connexion', function(req, res, next) {
+  
+  console.log("received : /connexion : "+JSON.stringify(req.params));
+  
+  if(!req.params.LOGIN || !req.params.PASS) {
+	res.send(new errs.BadRequestError('Missing LOGIN and/or PASS parameters'));
+  }else if(req.params.LOGIN == LOGIN && req.params.PASS == PASS){
+	res.send({TOKEN:TOKEN, ID:ID_CHAUFFEUR});
+  }else {
+	res.send(new errs.UnauthorizedError("Bad credentials"));
+  }
+  return next();
+});
+
+server.post('/deconnexion', function(req, res, next) {
+  
+  console.log("received : /deconnexion : "+JSON.stringify(req.params));
+  
+  if(!req.params.TOKEN) {
+	res.send(new errs.BadRequestError('Missing TOKEN parameters'));
+  }else {
+	res.send({});
+  }
+  return next();
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
